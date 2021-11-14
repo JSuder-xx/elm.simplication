0d5aa32fceb67586a0e3d2c95974450ee6ce55c1
JSuder-xx <j.j.suder@gmail.com>
2021-11-14 18:17:04 -0500

    Bump release

--- dev-build.sh
+++ dev-build.sh
@@ -1,3 +1,3 @@
-elm make src/Main.elm --optimize --output=dist/simplication.0.1.5.min.js
+elm make src/Main.elm --optimize --output=dist/simplication.0.1.6.min.js
 cp node_modules/viz.js/viz.js dist/viz.js
 cp node_modules/viz.js/full.render.js dist/full.render.js
